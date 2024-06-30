acb4e8e69409315bc19be1f0ca7e0b53076c57ef
Andrianov Roman <r_andrianov02@mail.ru>
2024-06-30 20:19:30 +0300

    Create repo with crud for project

---
+++ src/main/java/com/example/studioJudo/StudioJudoApplication.java
@@ -0,0 +1,13 @@
+package com.example.studioJudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioJudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioJudoApplication.class, args);
+	}
+
+}
